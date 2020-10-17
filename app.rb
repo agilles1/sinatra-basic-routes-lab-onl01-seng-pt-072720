57b885f2ad4424259970db2eba6cba5fd9562e2c
agilles1 <adam.g.tpt@gmail.com>
2020-10-17 13:52:29 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/' do 
+        "Hello. World!"
+    end
+
+    get '/name' do
+        "My name is Adam Gillespie"
+    end
+
+    get '/hometown' do
+        "My hometown is harleston, WV"
+    end
+
+    get '/favorite-song' do
+        'My favorite song is Strauss - "Morgen"'
+    end
 end
